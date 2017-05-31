fc531188e4e3d720028c11f2ee501d80775ae19f
Andrea Marini <andrea.marini@cnr.it>
2017-05-31 10:54:48 +0200

    Robot doncamillo/marini added

--- test-suite/ROBOTS/doncamillo/marini/CONFIGURATIONS/default.sh
+++ test-suite/ROBOTS/doncamillo/marini/CONFIGURATIONS/default.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ./configure \
+  FPP="gfortran -E -P -cpp" --enable-par-linalg \
   --with-blas-libs="-lmkl_intel_lp64  -lmkl_sequential -lmkl_core" \
   --with-lapack-libs="-lmkl_intel_lp64  -lmkl_sequential -lmkl_core" \
   --with-fft-libs="-mkl " \
---
+++ test-suite/ROBOTS/doncamillo/marini/FLOWS/validate.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+#
+@flow = (
+{
+ ACTIVE      => "yes",
+ TESTS       => "all",
+ KEYS        => "all hard",
+},
+{
+ MPI_CPU     => $SYSTEM_NP,
+ PAR_MODE    => "default",
+},
+{
+ MPI_CPU     => $SYSTEM_NP,
+ PAR_MODE    => "random",
+},
+{
+ MPI_CPU     => 4,
+ PAR_MODE    => "loop",
+},
+{
+ THREADS     => $SYSTEM_NP,
+},
+{
+ MPI_CPU     => $SYSTEM_NP,
+ SLK_CPU     => 4,
+ PAR_MODE    => "default",
+},
+{
+ MPI_CPU     => 8,
+ THREADS     => 2,
+ SLK_CPU     => 4,
+ PAR_MODE    => "default",
+},
+);
