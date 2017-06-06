fe4ed2fe0fefd02ca3d0b96d5dd93879c0eb3895
Andrea Marini <andrea.marini@cnr.it>
2017-06-06 16:10:12 +0200

    Potzie/marini ROBOT added

---
+++ test-suite/ROBOTS/potzie/marini/FLOWS/validate.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+#
+@flow = (
+{
+ ACTIVE      => "yes",
+ CONFIG      => "default.sh",
+ TESTS       => "all",
+ KEYS        => "all hard",
+},
+{
+ ACTIVE      => "no",
+ MPI_CPU     => $SYSTEM_NP,
+ PAR_MODE    => "default",
+},
+{
+ MPI_CPU     => $SYSTEM_NP,
+ PAR_MODE    => "random",
+},
+{
+ MPI_CPU     => 4,
+ PAR_MODE    => "loop",
+},
+{
+ THREADS     => $SYSTEM_NP,
+},
+{
+ MPI_CPU     => $SYSTEM_NP,
+ SLK_CPU     => 4,
+ PAR_MODE    => "default",
+},
+{
+ MPI_CPU     => 8,
+ THREADS     => 2,
+ SLK_CPU     => 4,
+ PAR_MODE    => "default",
+},
+);
