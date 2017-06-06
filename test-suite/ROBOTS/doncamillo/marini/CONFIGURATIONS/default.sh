c0a45a7b9ed203d00e39cdf0baa5f31588761c65
Andrea Marini <andrea.marini@cnr.it>
2017-06-06 14:37:49 +0200

    Doncamillo ROBOT using Intel up and running

--- test-suite/ROBOTS/doncamillo/marini/CONFIGURATIONS/default.sh
+++ test-suite/ROBOTS/doncamillo/marini/CONFIGURATIONS/default.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
-
+IF_COMPILE=`which ifort`
+FC=gfortran 
+if [ -e "$IF_COMPILE" ]
+then
+ FC=ifort 
+fi
 ./configure \
-  FPP="gfortran -E -P -cpp" --enable-par-linalg \
-  --with-blas-libs="-lmkl_intel_lp64  -lmkl_sequential -lmkl_core" \
-  --with-lapack-libs="-lmkl_intel_lp64  -lmkl_sequential -lmkl_core" \
-  --with-fft-libs="-mkl " \
-  --enable-time-profile \
-  --enable-open-mp \
-  --enable-msgs-comps
-
+FC=$FC \
+--enable-memory-profile=yes \
+--enable-int-linalg=yes \
+--enable-par-linalg=yes \
+--enable-time-profile=yes \
+--enable-msgs-comps=yes \
+--enable-open-mp=yes \
+--enable-keep-src \
+--with-blacs-libs=yes \
+--with-scalapack-libs=yes
+#--with-blacs-libs="$BLACS_LIB" \
+#--with-scalapack-libs="$SCALAPACK_LIB"
+#--with-extlibs-path=$YAMBO_EXT_LIBS \
+#--with-iotk-libdir=$IOTK_LIB \
+#--with-iotk-includedir=$IOTK_INCLUDE \
+#--with-libxc-libdir=$LIBXC_LIB \
+#--with-libxc-includedir=$LIBXC_INCLUDE \
+#--with-blas-libs="$BLAS_LIB" \
+#--with-lapack-libs="$LAPACK_LIB" \
+#--with-netcdf-libdir=$NETCDF_LIB \
+#--with-netcdf-includedir=$NETCDF_INCLUDE \
+#--with-fft-path=$FFT_PATH \
+#--with-fft-libdir=$FFT_LIB \
+#--with-fft-includedir=$FFT_INCLUDE \
+#--with-petsc-libs=$PETSC_LIB \
+#--with-slepc-libs=$SLEPC_LIB \
