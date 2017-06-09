19098b77d787a70bee52805920f44dcb8bf1a6e7
Andrea Marini <andrea.marini@cnr.it>
2017-06-09 14:53:38 +0200

    ROBOT marini@narro installed

---
+++ test-suite/ROBOTS/narro/marini/FLOWS/validate.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+#
+@flow = (
+{
+ ACTIVE      => "yes",
+ CONFIG      => "default.sh",
+ TESTS       => "hBN/GW-OPTICS",
+ KEYS        => "all hard",
+},
+{
+ ACTIVE      => "yes",
+ MPI_CPU     => $SYSTEM_NP,
+ PAR_MODE    => "default",
+},
+{
+ ACTIVE      => "no",
+ MPI_CPU     => $SYSTEM_NP,
+ PAR_MODE    => "random",
+},
+{
+ MPI_CPU     => $SYSTEM_NP_half,
+ PAR_MODE    => "loop",
+},
+{
+ THREADS     => $SYSTEM_NP_half,
+},
+{
+ MPI_CPU     => $SYSTEM_NP,
+ SLK_CPU     => $SYSTEM_NP_half,
+ PAR_MODE    => "default",
+},
+{
+ MPI_CPU     => $SYSTEM_NP_half,
+ THREADS     => $SYSTEM_NP_half,
+ SLK_CPU     => $SYSTEM_SLK,
+ PAR_MODE    => "default",
+},
+);
