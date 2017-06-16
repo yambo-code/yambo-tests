18ce5aa22a395e3a564faf8748ee73d5152f80f9
Andrea Marini <andrea.marini@cnr.it>
2017-06-16 09:38:14 +0200

    Al_Bulk REFERENCES updated and added tiny temperature to SC Hartree

--- test-suite/ROBOTS/anomaly.mlib.ism.cnr.it/marini/FLOWS/failed_devel-qp-dbs.pl
+++ test-suite/ROBOTS/anomaly.mlib.ism.cnr.it/marini/FLOWS/failed_devel-qp-dbs.pl
@@ -3,27 +3,6 @@
 {
 KEYS        => 'all hard',
 ACTIVE      => 'yes',
-TESTS     => 'Si_bulk/RT/ all ;Si_bulk/SC/ all ;WSe2/RT/ all ;Iron/pwscf/ all ;MoS2/pwscf/RT/ all ; ',
-},
-{
-KEYS        => 'all hard',
-MPI_CPU     => '8',
-PAR_MODE     => 'default',
-ACTIVE      => 'no',
-TESTS     => 'Si_bulk/RT/ all ;Si_bulk/SC/ all ;WSe2/RT/ all ;Iron/pwscf/ all ;MoS2/pwscf/RT/ all ; ',
-},
-{
-KEYS        => 'all hard',
-THREADS     => '2',
-ACTIVE      => 'no',
-TESTS     => 'Si_bulk/RT/ all ;Si_bulk/SC/ all ;WSe2/RT/ all ;Iron/pwscf/ all ;MoS2/pwscf/RT/ all ; ',
-},
-{
-KEYS        => 'all hard',
-MPI_CPU     => '8',
-SLK_CPU     => '4',
-PAR_MODE     => 'default',
-ACTIVE      => 'no',
-TESTS     => 'Si_bulk/RT/ all ;Si_bulk/SC/ all ;WSe2/RT/ all ;Iron/pwscf/ all ;MoS2/pwscf/RT/ all ; ',
+TESTS     => 'Si_bulk/RT/ all ;WSe2/RT/ all ;Al_bulk/GW-OPTICS/ all ;MoS2/pwscf/RT/ all ;hBN/RT/ all ; ',
 },
 );
