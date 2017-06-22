1fff336a924728077dcb5db5b47ff9832ad10825
Andrea Marini <andrea.marini@cnr.it>
2017-06-22 18:20:36 +0200

    Added line to work with latest perl version

--- test-suite/driver.pl
+++ test-suite/driver.pl
@@ -23,6 +23,7 @@
 # License along with this program; if not, write to the Free
 # Software Foundation, Inc., 59 Temple Place - Suite 330,Boston,
 #
+use lib ".";
 do "config/MODULES.pl";
 do "config/TOOLS.pl";
 #
