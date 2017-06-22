a62ecb84b4728e63cc75a1fce3042eb6e18f8ad5
Andrea Marini <andrea.marini@cnr.it>
2017-06-22 18:38:16 +0200

    Polluce ROBOT created

---
+++ test-suite/ROBOTS/polluce.mlib.ism.cnr.it/marini/FLOWS/validate.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+#
+@flow = (
+{
+ ACTIVE      => "yes",
+ CONFIG      => "default.sh",
+ KEYS        => "all hard",
+},
+{
+ MPI_CPU     => 8,
+ PAR_MODE    => "default",
+},
+{
+ MPI_CPU     => 8,
+ PAR_MODE    => "random",
+},
+{
+ MPI_CPU     => 4,
+ PAR_MODE    => "loop",
+},
+{
+ THREADS     => 4,
+},
+{
+ MPI_CPU     => 8,
+ SLK_CPU     => 4,
+ PAR_MODE    => "default",
+},
+{
+ MPI_CPU     => 8,
+ THREADS     => 2,
+ SLK_CPU     => 4,
+ PAR_MODE    => "default",
+},
+);
