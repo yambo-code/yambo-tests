e4d9731eef4ac1e20df2d824a9c2c157ea181ee7
Andrea Marini <andrea.marini@cnr.it>
2017-09-19 11:43:19 +0200

    driver library and connected files removed

--- test-suite/ROBOTS/anomaly.mlib.ism.cnr.it/marini/FLOWS/failed_devel-qp-dbs.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-@flow = (
-{
-KEYS        => 'all hard',
-ACTIVE      => 'yes',
-TESTS     => 'Si_bulk/RT/ all ;WSe2/RT/ all ;Al_bulk/GW-OPTICS/ all ;MoS2/pwscf/RT/ all ;hBN/RT/ all ; ',
-},
-);
