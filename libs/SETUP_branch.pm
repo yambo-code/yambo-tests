d6e31568192688aa9728a62561602566a73f3ac8
Fulvio PALEARI <fulvio.paleari@uni.lu>
2018-05-14 10:11:55 +0200

    Added tests on NEQ Fermi occupations for hBN, Si, MoS2 (branch devel-ypp-rt)

--- libs/SETUP_branch.pm
+++ libs/SETUP_branch.pm
@@ -89,6 +89,7 @@ if ($pattern=~m/4.0/ix) {undef $is_NEW_YPP};
 if ($pattern=~m/4.1/ix) {undef $is_NEW_YPP};
 if ($pattern=~m/4.2/ix) {undef $is_NEW_YPP};
 if ($is_GPL) {undef $is_NEW_YPP};
+if ($pattern=~m/devel-ypp-rt/ix) {undef $is_NEW_YPP};
 #
 # Define list of required executables
 $target_list = $target_list_basic; 
