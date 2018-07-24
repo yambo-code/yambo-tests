1818367e7f6a0c82c69203545762b9b5d9efcba2
Davide Sangalli <davide.sangalli@gmail.com>
2018-07-24 15:21:36 +0200

    devel-slepc can run NL tests

--- libs/SETUP_branch.pm
+++ libs/SETUP_branch.pm
@@ -85,6 +85,7 @@ if ($pattern=~m/devel-rt-rotate/ix)      {$do_NL_tests="yes"};
 if ($pattern=~m/develop/ix)      {$do_NL_tests="yes"};
 if ($pattern=~m/devel-cutoff/ix) {$do_NL_tests="yes"};
 if ($pattern=~m/devel-current-approach/ix) {$do_NL_tests="yes"};
+if ($pattern=~m/devel-slepc/ix) {$do_NL_tests="yes"};
 if ($pattern=~m/master/ix && $is_GPL)      {$do_NL_tests="yes"};
 #
 if ($is_GPL) {$branch_key.="_gpl"};
