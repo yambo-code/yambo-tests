fe99db196d8be72c238f1a80abfa9e580d320136
marini <andrea.marini@cnr.it>
2018-08-03 14:13:44 +0200

    Reference files for devel-rt-rotate added

--- ROBOTS/anomaly.mlib.ism.cnr.it/marini/FLOWS/devel-rt-rotate.pl
+++ ROBOTS/anomaly.mlib.ism.cnr.it/marini/FLOWS/devel-rt-rotate.pl
@@ -10,11 +10,11 @@ ACTIVE      => 'no',
 TESTS     => 'Beryllium/YPP'
 },
 {
-ACTIVE      => 'no',
+ACTIVE      => 'yes',
 TESTS     => 'MoS2/pwscf/RT 00_* 01_* 02_* 03_bands_interp 07_ypp_bands 07_ypp_dos_occ 08_ypp_bands_DbGd 08_ypp_dos_occ_DbGd'
 },
 {
-ACTIVE      => 'yes',
+ACTIVE      => 'no',
 TESTS     => 'Si_bulk/RT 00_* 01_* 02_carriers_DB_ypp 02_occ_bands_elph_0K 06_carriers_DB_ypp_DbGd 06_occ_bands_elph_0K_DbGd'
 },
 {
