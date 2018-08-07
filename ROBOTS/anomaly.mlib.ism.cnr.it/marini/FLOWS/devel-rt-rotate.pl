661202688186df8a5b14b045c987ef80ab6c0555
marini <andrea.marini@cnr.it>
2018-08-07 22:31:24 +0200

    Input files updated

--- ROBOTS/anomaly.mlib.ism.cnr.it/marini/FLOWS/devel-rt-rotate.pl
+++ ROBOTS/anomaly.mlib.ism.cnr.it/marini/FLOWS/devel-rt-rotate.pl
@@ -2,11 +2,11 @@
 @flow = (
 {
 KEYS        => 'elph rt nopj hard',
-ACTIVE      => 'no',
+ACTIVE      => 'yes',
 TESTS     => 'hBN/YPP'
 },
 {
-ACTIVE      => 'no',
+ACTIVE      => 'yes',
 TESTS     => 'Beryllium/YPP'
 },
 {
@@ -14,11 +14,11 @@ ACTIVE      => 'yes',
 TESTS     => 'MoS2/pwscf/RT 00_* 01_* 02_* 03_bands_interp 07_ypp_bands 07_ypp_dos_occ 08_ypp_bands_DbGd 08_ypp_dos_occ_DbGd'
 },
 {
-ACTIVE      => 'no',
+ACTIVE      => 'yes',
 TESTS     => 'Si_bulk/RT 00_* 01_* 02_carriers_DB_ypp 02_occ_bands_elph_0K 06_carriers_DB_ypp_DbGd 06_occ_bands_elph_0K_DbGd'
 },
 {
-ACTIVE      => 'no',
+ACTIVE      => 'yes',
 TESTS     => 'Si_bulk/ELPH/QP_CTL',
 }
 );
