b12e0bcab99cfaeb5f16e1e45cbc51b9845622df
Andrea Marini <marini@amore9.mlib.ism.cnr.it>
2018-09-12 17:07:48 +0200

    Bug fix in PHP upload due to wrong ordering in UTILS_backup.pm

--- libs/UTILS_backup.pm
+++ libs/UTILS_backup.pm
@@ -50,8 +50,12 @@ sub UTILS_list_backups{
  @sorted_dirs = sort { $a1 = (split ( '2017', $a )) [1]; $b1 = (split ( '2017', $b )) [1]; $a1 cmp $b1} @dirs_to_process_2017;
  @dirs_2018   = sort { $a1 = (split ( '2018', $a )) [1]; $b1 = (split ( '2018', $b )) [1]; $a1 cmp $b1} @dirs_to_process_2018;
  push(@sorted_dirs, @dirs_2018);
- @reversed_dirs = @sorted_dirs ;
- #@reversed_dirs = reverse @sorted_dirs ;
+ if ($php) 
+ {
+  @reversed_dirs = reverse @sorted_dirs
+ }else{
+  @reversed_dirs = @sorted_dirs 
+ }
  #
  $data_id=0;
  if ("@_" eq "list")
