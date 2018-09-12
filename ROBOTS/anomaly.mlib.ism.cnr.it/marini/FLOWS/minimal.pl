cc0ae9ce6d9e23c3b098937823f8cf011fa13842
marini <andrea.marini@cnr.it>
2018-09-12 17:08:37 +0200

    - Added AVERAGE rules for RT files in MoS2 - Fixed error in 04_init.actions that was causing a wrong restart in a flow loop

--- ROBOTS/anomaly.mlib.ism.cnr.it/marini/FLOWS/minimal.pl
+++ ROBOTS/anomaly.mlib.ism.cnr.it/marini/FLOWS/minimal.pl
@@ -2,16 +2,16 @@
 #
 @flow = (
 {
- ACTIVE      => "no",
+ ACTIVE      => "yes",
 # CPU_CONF     => "SAMPLE_conf.cpu",
 # CONFIG      => "default.sh",
-# TESTS       => "MoS2/pwscf/RT 00_init 01_fix_symm 02_init",
+ TESTS       => "MoS2/pwscf/RT 00_init 01_fix_symm 02_init 06_td_sex 06_td_sex_on_the_fly",
 # TESTS       => "Iron/abinit/With-SOC 01_init 02_fix_symm 03_init 04_IP-RPA_len",
 # TESTS       => "Si_bulk/MAGNETIC; Si_bulk/ELPH/QP_CTL; Si_bulk/ELPH/base_for_BSE; Si_bulk/ELPH/OPTICS; Si_bulk/GW-OPTICS; Si_bulk/RT; C6H3Cl3",
- TESTS       => "Al111 01_init 02_eels",
-# TESTS       => "Al_bulk/GW-OPTICS",
+#TESTS       => "Al111 01_init 02_eels",
+#TESTS       => "H2/RT 00_init 00_ypp 01_init 04_init 05_alda",
 # KEYS        => "all hard",
-# KEYS        => "rt hard",
+ KEYS        => "rt hard",
 },
 {
  ACTIVE      => "no",
@@ -20,15 +20,15 @@
  PAR_MODE    => "default",
 },
 {
- MPI_CPU     => 4,
-# MPI_CPU     => $SYSTEM_NP,
- PAR_MODE    => "default",
-},
-{
  ACTIVE      => "yes",
  MPI_CPU     => 8,
 # MPI_CPU     => $SYSTEM_NP,
  PAR_MODE    => "default",
+},
+{
+ MPI_CPU     => 8,
+# MPI_CPU     => $SYSTEM_NP,
+ PAR_MODE    => "random",
 },
 {
  ACTIVE      => "no",
