0c28623b5efc164b768f4c2e14ac9ab46fe00bf5
Davide Sangalli <davide.sangalli@gmail.com>
2018-12-18 02:41:58 +0100

    Dipoles parallelization in test-suite

--- libs/RUN_load_PAR_fields.pm
+++ libs/RUN_load_PAR_fields.pm
@@ -59,7 +59,7 @@ if ( $default_parallel ){
 #
 # PARALLEL section
 #
-$PAR_field[0]="DIP_ROLEs= \"c.v\""; #  => GKCV
+$PAR_field[0]="DIP_ROLEs= \"k.c.v\""; #  => GKCV
 $PAR_field[1]="X_q_0_ROLEs= \"g.k.c.v\""; #  => GKCV
 $PAR_field[2]="X_finite_q_ROLEs= \"g.q.k.c.v\""; # => GQKCV
 $PAR_field[3]="X_all_q_ROLEs= \"g.q.k.c.v\""; # => GQKCV
