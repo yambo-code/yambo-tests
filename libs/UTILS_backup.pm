8dfbac41bbd709dee67392f5f1f97ab1443ee696
Davide Sangalli <davide.sangalli@gmail.com>
2019-01-10 09:35:18 +0100

    - Test suite now uploads also result in 2019 folder - Prepared new yambo_sc test (BROKEN for now)

--- libs/UTILS_backup.pm
+++ libs/UTILS_backup.pm
@@ -40,6 +40,9 @@ sub UTILS_list_backups{
   if ($dir =~ /\/2018\//) {
    push @dirs_to_process_2018, $dir;
   }
+  if ($dir =~ /\/2019\//) {
+   push @dirs_to_process_2019, $dir;
+  }
   $n_backups++;
  }
  #
@@ -49,7 +52,8 @@ sub UTILS_list_backups{
  };
  @sorted_dirs = sort { $a1 = (split ( '2017', $a )) [1]; $b1 = (split ( '2017', $b )) [1]; $a1 cmp $b1} @dirs_to_process_2017;
  @dirs_2018   = sort { $a1 = (split ( '2018', $a )) [1]; $b1 = (split ( '2018', $b )) [1]; $a1 cmp $b1} @dirs_to_process_2018;
- push(@sorted_dirs, @dirs_2018);
+ @dirs_2019   = sort { $a1 = (split ( '2019', $a )) [1]; $b1 = (split ( '2019', $b )) [1]; $a1 cmp $b1} @dirs_to_process_2019;
+ push(@sorted_dirs, @dirs_2018, @dirs_2019);
  if ($branch_php or $report) 
  {
   @reversed_dirs = reverse @sorted_dirs
