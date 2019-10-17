42831e99a071cb76407f0f4fef96c88e6d82c9aa
Andrea Marini <andrea.marini@cnr.it>
2019-10-17 09:54:40 +0200

    Added branch independent scripts to simplify the new branch checking

---
+++ ROBOTS/castore.mlib.ism.cnr.it/marini/FLOWS/failed.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+@flow = (
+{
+KEYS        => 'all hard',
+ACTIVE      => 'yes',
+TESTS     => 'Si_bulk/RT/ all ;Si_bulk/MAGNETIC/ all ;hBN/SC/ all ;C6H3Cl3/ all ; ',
+}
+)
+;
--- ROBOTS/castore.mlib.ism.cnr.it/marini/FLOWS/failed_devel-long-options-merge-ready.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-@flow = (
-{
-KEYS        => 'all hard',
-ACTIVE      => 'yes',
-TESTS     => 'Si_bulk/RT/ all ;Si_bulk/MAGNETIC/ all ;MoS2/pwscf/RT/ all ; ',
-},
-);
