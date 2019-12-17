d9aaa87c944c410a21f15c1808e8ba20eb022102
Andrea Marini <andrea.marini@cnr.it>
2019-12-17 12:19:20 +0100

    Robots clean-up. Dead/un-configured moved to OUT-OF-DATE

--- ROBOTS/dufour.mlib.ism.cnr.it/marini/FLOWS/failed_devel-sc.pl
+++ ROBOTS/dufour.mlib.ism.cnr.it/marini/FLOWS/failed_devel-sc.pl
@@ -3,7 +3,7 @@
 {
 KEYS        => 'all hard',
 ACTIVE      => 'yes',
-TESTS     => 'Si_bulk/MAGNETIC/ all ;PA_chain/ all ; ',
+TESTS     => 'PA_chain/ all ; ',
 },
 {
 KEYS        => 'all hard',
@@ -17,7 +17,7 @@ KEYS        => 'all hard',
 MPI_CPU     => '8',
 PAR_MODE     => 'random',
 ACTIVE      => 'yes',
-TESTS     => 'Si_bulk/RT/ all ;Si_bulk/GW-OPTICS/ all ;PA_chain/ all ;MoS2/pwscf/RT/ all ;MoS2/abinit/OPTICS/Without-SOC-sp2/ all ;MoS2/abinit/OPTICS/Without-SOC-sp1/ all ;LiF/GW-OPTICS/ all ;hBN/SC/ all ;hBN/RT/ all ;hBN/GW-OPTICS/ all ;H_chain/2.5/ all ;H_chain/2.05/ all ;Al_bulk/GW-OPTICS/ all ; ',
+TESTS     => 'Si_bulk/RT/ all ;Si_bulk/GW-OPTICS/ all ;PA_chain/ all ;MoS2/pwscf/RT/ all ;MoS2/pwscf/OPTICS/Without-SOC-sp2/ all ;MoS2/abinit/OPTICS/Without-SOC-sp1/ all ;LiF/GW-OPTICS/ all ;hBN/SC/ all ;hBN/RT/ all ;hBN/GW-SHIFTED/SHIFTED/ all ;hBN/GW-SHIFTED/REGULAR/ all ;hBN/GW-OPTICS/ all ;H_chain/2.5/ all ;H_chain/2.05/ all ;Al_bulk/GW-OPTICS/ all ; ',
 },
 {
 KEYS        => 'all hard',
@@ -25,7 +25,7 @@ MPI_CPU     => '8',
 SLK_CPU     => '4',
 PAR_MODE     => 'random',
 ACTIVE      => 'yes',
-TESTS     => 'Si_bulk/SC/ all ;Si_bulk/RT/ all ;PA_chain/ all ;MoS2/pwscf/RT/ all ;MoS2/pwscf/OPTICS/Without-SOC-sp2/ all ;MoS2/abinit/OPTICS/Without-SOC-sp1/ all ;LiF/GW-OPTICS/ all ;hBN/SC/ all ;hBN/RT/ all ;hBN/GW-OPTICS/ all ;H_chain/2.5/ all ;H_chain/2.05/ all ;Al_bulk/GW-OPTICS/ all ; ',
+TESTS     => 'Si_bulk/RT/ all ;Si_bulk/GW-OPTICS/ all ;PA_chain/ all ;MoS2/pwscf/OPTICS/With-SOC/ all ;MoS2/abinit/OPTICS/Without-SOC-sp1/ all ;hBN/SC/ all ;hBN/RT/ all ;hBN/GW-SHIFTED/SHIFTED/ all ;hBN/GW-SHIFTED/REGULAR/ all ;hBN/GW-OPTICS/ all ;H_chain/2.5/ all ;H_chain/2.05/ all ;AlAs/RT/ all ;Al_bulk/GW-OPTICS/ all ; ',
 },
 {
 KEYS        => 'all hard',
---
+++ ROBOTS/dufour.mlib.ism.cnr.it/marini/SCRIPTS/script_GF_MPICH.tcsh
@@ -0,0 +1,7 @@
+#!/usr/bin/tcsh
+#cd yambo-tests
+module purge
+module load full-suite/gcc/8.2.0/mpich-3.2.1
+#./driver.pl -flow validate -report -branch $1 -nice -newer 7 -safe -input
+./driver.pl -flow failed_devel-sc -report -branch $1 -nice -newer 7 -safe -input
+
