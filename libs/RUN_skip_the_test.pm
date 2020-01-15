32852cfae684c941b225e8a7785f27a5109a44a8
Davide Sangalli <davide.sangalli@gmail.com>
2020-01-15 01:02:54 +0100

    Partially reverting last commit

--- libs/RUN_skip_the_test.pm
+++ libs/RUN_skip_the_test.pm
@@ -36,7 +36,7 @@ if ( "@_" eq "DIR" ){
  } elsif ($project ne "all") {
   if (-e "PL" && $project !~ /pl/) { $message=" skipped (wrong PJ)"};
   if (-e "NL" && $project !~ /nl/) {$message=" skipped (wrong PJ)"};
-  #if (-e "NL" && $Yambo_precision !~ /double/) {$message=" skipped (yambo SP compile)"};
+  if (-e "NL" && $Yambo_precision !~ /double/) {$message=" skipped (yambo SP compile)"};
   if (-e "NL" && not $do_NL_tests ) {$message=" skipped (wrong BRANCH)"};
   if (-e "QED" && $project !~ /qed/) {$message=" skipped (wrong PJ)"};
   if (-e "RT" && $project !~ /rt/) {$message=" skipped (wrong PJ)"};
