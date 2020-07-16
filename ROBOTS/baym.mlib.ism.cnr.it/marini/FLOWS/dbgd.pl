40c4f388c1dd25541ab7120b62ab8860751faeae
Andrea Marini <andrea.marini@cnr.it>
2020-07-16 17:58:30 +0200

    REFERENCE_devel-andreaM updated. To check them carefully.

---
+++ ROBOTS/baym.mlib.ism.cnr.it/marini/FLOWS/dbgd.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+@flow = (
+{
+KEYS        => 'all',
+ACTIVE      => 'yes',
+TESTS     => 'hBN/RT;MoS2/pwscf/RT;Si_bulk/RT;Si_bulk/GW-OPTICS;Si_bulk/ELPH/base_for_BSE;Si_bulk/ELPH/OPTICS;'
+},
+);
