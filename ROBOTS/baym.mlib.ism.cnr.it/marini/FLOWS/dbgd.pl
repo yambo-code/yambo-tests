360003807808d6acac0ed389dc4d2dc86f97172b
Andrea Marini <andrea.marini@cnr.it>
2020-07-24 15:28:32 +0200

    REFERENCE_devel-andreaM added

--- ROBOTS/baym.mlib.ism.cnr.it/marini/FLOWS/dbgd.pl
+++ ROBOTS/baym.mlib.ism.cnr.it/marini/FLOWS/dbgd.pl
@@ -3,12 +3,14 @@
 {
 KEYS        => 'rt',
 ACTIVE      => 'no',
-TESTS     => 'hBN/RT 01* 02_collisions_sex 08* 09* 10* 11* 12* 13*',
+TESTS     => 'hBN/RT',
+#TESTS     => 'hBN/RT 01* 02_collisions_sex 08* 09* 10* 11* 12* 13*',
 },
 {
 KEYS        => 'rt',
 ACTIVE      => 'no',
-TESTS     => 'Si_bulk/RT 00_init 01* 05* 06_elph_0K_DbGd 06_carriers_DB_ypp_DbGd 06_fit_elph_0K_DbGd 06_occ_bands_elph_0K_DbGd',
+TESTS     => 'Si_bulk/RT',
+#TESTS     => 'Si_bulk/RT 00_init 01* 05* 06_elph_0K_DbGd 06_carriers_DB_ypp_DbGd 06_fit_elph_0K_DbGd 06_occ_bands_elph_0K_DbGd',
 },
 {
 KEYS        => 'rt',
