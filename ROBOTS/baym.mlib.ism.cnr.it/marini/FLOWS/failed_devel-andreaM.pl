b5ae9b648d8bd9ba2501b16c79bb7a73f225febf
Andrea Marini <andrea.marini@cnr.it>
2020-07-29 14:26:53 +0200

    REFERENCES devel-andreaM updated

--- ROBOTS/baym.mlib.ism.cnr.it/marini/FLOWS/failed_devel-andreaM.pl
+++ ROBOTS/baym.mlib.ism.cnr.it/marini/FLOWS/failed_devel-andreaM.pl
@@ -1,15 +1,23 @@
 #!/usr/bin/perl
 @flow = (
 {
-KEYS        => 'all hard',
+KEYS        => 'rt hard',
+#MPI_CPU     => '8',
 ACTIVE      => 'yes',
-TESTS     => 'MoS2/pwscf/RT/ all ;MoS2/P2Y/ all ;hBN/RT/ all ; ',
+TESTS     => 'Si_bulk/RT 00_init 01_fix_symm 01_init 11_fix_symm 11_init 13_elph_0K_field_ad',
 },
 {
-KEYS        => 'all hard',
+KEYS        => 'rt hard',
 MPI_CPU     => '8',
 PAR_MODE     => 'default',
 ACTIVE      => 'no',
-TESTS     => 'Si_bulk/RT/ all ;Si_bulk/GW-OPTICS/ all ;MoS2/pwscf/RT/ all ;MoS2/P2Y/ all ;hBN/RT/ all ; ',
+TESTS     => 'Si_bulk/RT 00_init 01_fix_symm 01_init 02_carriers_DB_ypp 02_elph_0K 02_elph_300K 03_coll_elel 04_elel+elph_0K',
+},
+{
+KEYS        => 'rt hard',
+MPI_CPU     => '8',
+PAR_MODE     => 'default',
+ACTIVE      => 'no',
+TESTS     => 'Si_bulk/RT 00_init 01_fix_symm 01_init 05_E_DbGd 06_carriers_DB_ypp_DbGd 06_elph_0K 06_plot_elph_0K_DbGd',
 },
 );
