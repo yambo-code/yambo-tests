5330da2b09f3ac8442ddb4a54cad7b6449ff6673
Andrea Marini <andrea.marini@cnr.it>
2020-07-30 16:37:04 +0200

    find_the_diff instructed to work on files with different numbers of data

--- ROBOTS/baym.mlib.ism.cnr.it/marini/FLOWS/failed_devel-andreaM.pl
+++ ROBOTS/baym.mlib.ism.cnr.it/marini/FLOWS/failed_devel-andreaM.pl
@@ -4,6 +4,12 @@
 KEYS        => 'rt hard',
 #MPI_CPU     => '8',
 ACTIVE      => 'yes',
+TESTS     => 'MoS2/pwscf/RT 00_init 01_fix_symm 02_init 03_map_grid 05_collisions 07_ep 08_ep_DbGd',
+},
+{
+KEYS        => 'rt hard',
+#MPI_CPU     => '8',
+ACTIVE      => 'no',
 TESTS     => 'Si_bulk/RT 00_init 01_fix_symm 01_init 11_fix_symm 11_init 13_elph_0K_field_ad',
 },
 {
