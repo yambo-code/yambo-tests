608623186bcf2a7dcdf256b438d25731575fe139
Andrea Marini <andrea.marini@cnr.it>
2020-10-14 13:05:20 +0200

    Input file + Refs updated

---
+++ ROBOTS/baym/marini/FLOWS/failed.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+@flow = (
+{
+KEYS        => 'rt',
+#MPI_CPU     => '8',
+#PAR_MODE     => 'random',
+ACTIVE      => 'yes',
+TESTS     => 'hBN/RT/ all ;Si_bulk/RT/ all ;MoS2/pwsc/RT/ all',
+},
+);
