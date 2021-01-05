88cbeb8444e155b693810c70f1f257240f86f4f6
Davide Sangalli <davide.sangalli@gmail.com>
2021-01-05 10:22:42 +0100

    Fix for issue #19. Year 2021 was missing frim list of folders

--- libs/UTILS_backup.pm
+++ libs/UTILS_backup.pm
@@ -26,6 +26,7 @@ sub UTILS_list_backups{
  my $n_backups_to_save=50;
  my $n_backups=0;
  my @dir = ( "backup_and_www/$host/$user/" );
+ print "checking folder backup_and_www/$host/$user/";
  if ($mode eq "bench") {@dir="benchmark-results/"};
  my @dirs;
  find( sub { push @dirs, $File::Find::name if -d }, @dir );
@@ -54,6 +55,11 @@ sub UTILS_list_backups{
    push @dirs_to_process_2020, $dir;
    $n_backups++;
   }
+  if ($dir =~ /\/2021\//) {
+   #print "check 2021: $dir\n";
+   push @dirs_to_process_2021, $dir;
+   $n_backups++;
+  }
  }
  #
  if ($clean and $backup_logs eq "yes") {
@@ -64,7 +70,8 @@ sub UTILS_list_backups{
  @dirs_2018   = sort { $a1 = (split ( '2018', $a )) [1]; $b1 = (split ( '2018', $b )) [1]; $a1 cmp $b1} @dirs_to_process_2018;
  @dirs_2019   = sort { $a1 = (split ( '2019', $a )) [1]; $b1 = (split ( '2019', $b )) [1]; $a1 cmp $b1} @dirs_to_process_2019;
  @dirs_2020   = sort { $a1 = (split ( '2020', $a )) [1]; $b1 = (split ( '2020', $b )) [1]; $a1 cmp $b1} @dirs_to_process_2020;
- push(@sorted_dirs, @dirs_2018, @dirs_2019, @dirs_2020);
+ @dirs_2021   = sort { $a1 = (split ( '2021', $a )) [1]; $b1 = (split ( '2021', $b )) [1]; $a1 cmp $b1} @dirs_to_process_2021;
+ push(@sorted_dirs, @dirs_2018, @dirs_2019, @dirs_2020, @dirs_2021);
  if ($branch_php or $report) 
  {
   @reversed_dirs = reverse @sorted_dirs
