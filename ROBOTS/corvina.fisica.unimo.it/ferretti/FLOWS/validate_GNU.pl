8b241855b6f989909a5ec2a7603c9b95591a507f
Andrea Ferretti <andrea.ferretti@nano.cnr.it>
2021-01-14 19:55:01 +0100

    updated

--- ROBOTS/corvina.fisica.unimo.it/ferretti/FLOWS/validate_GNU.pl
+++ ROBOTS/corvina.fisica.unimo.it/ferretti/FLOWS/validate_GNU.pl
@@ -6,20 +6,13 @@
  TESTS       => "all",
  CONFIG      => "gnu_SP.sh",
  KEYS        => "all hard",
-},
-{
  MPI_CPU     => 8,
  THREADS     => 2,
  PAR_MODE    => "default",
 },
 {
- MPI_CPU     => 2,
- THREADS     => 8,
- PAR_MODE    => "random",
-},
-{
  MPI_CPU     => 4,
  THREADS     => 4,
- PAR_MODE    => "loop",
+ PAR_MODE    => "random",
 },
 );
