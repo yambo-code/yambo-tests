cc9cc81e9f3b0e23d4f2f9a0d97fda53667da650
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2021-03-24 14:48:00 +0100

    Fixed default compilation

--- ROBOTS/attacc-lavoro/attacc/CONFIGURATIONS/default.sh
+++ ROBOTS/attacc-lavoro/attacc/CONFIGURATIONS/default.sh
@@ -8,8 +8,6 @@ export  LIBDIR=/usr/lib/x86_64-linux-gnu/
 --with-blas-libs="-lblas" --with-lapack-libs="-llapack"  --with-fft-libs=" -lfftw3" \
 --with-scalapack-libs="-L${LIBDIR} -lscalapack-openmpi" --with-blacs-libs="-L${LIBDIR} -lscalapack-openmpi" \
 --with-iotk-path="${YAMBO_LIBS}" --with-libxc-path="${YAMBO_LIBS}" \
---with-hdf5-includedir="${INCLUDEDIR}/hdf5/openmpi/" --with-hdf5-libdir="${LIBDIR}/hdf5/openmpi/" \
---with-netcdf-includedir="${INCLUDEDIR}"  --with-netcdf-libdir="${LIBDIR}" \
---with-netcdff-includedir="${INCLUDEDIR}" --with-netcdff-libdir="${LIBDIR}" \
+--with-hdf5-path="${YAMBO_LIBS}" --with-netcdf-path="${YAMBO_LIBS}" --with-netcdff-path="${YAMBO_LIBS}"\
 --enable-slepc-linalg --with-petsc-path="${YAMBO_LIBS}" --with-slepc-path="${YAMBO_LIBS}"
 
