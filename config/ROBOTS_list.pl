fa0d3fe0f055200fa4e1b086b5c7f7ae0dfb249e
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2021-09-16 11:04:45 +0200

    Added another PC

--- config/ROBOTS_list.pl
+++ config/ROBOTS_list.pl
@@ -50,3 +50,4 @@ $ROBOTS{'yurigagarin'} = 'yurigagarin.local';
 $ROBOTS{'attacc-lavoro'} = 'attacc-lavoro';
 $ROBOTS{'VivoBook'} = 'VivoBook';
 $ROBOTS{'AcerAspire'} = 'AcerAspire';
+$ROBOTS{'pc-tsn10'} = 'pc-tsn10';
