6a040552c481f8a0eeaadf9271b567a2ff307179
Andrea Ferretti <andrea.ferretti@nano.cnr.it>
2021-11-30 18:57:43 +0100

    corvina added

--- config/ROBOTS_list.pl
+++ config/ROBOTS_list.pl
@@ -47,3 +47,4 @@ $ROBOTS{'yurigagarin'} = 'yurigagarin.local';
 $ROBOTS{'attacc-lavoro'} = 'attacc-lavoro';
 $ROBOTS{'VivoBook'} = 'VivoBook';
 $ROBOTS{'AcerAspire'} = 'AcerAspire';
+$ROBOTS{'corvina'} = 'corvina.fisica.unimo.it';
