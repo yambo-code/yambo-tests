7f30d7fd64a7bde8d2b3988cbfa6c40d22d3025b
Andrea Ferretti <andrea.ferretti@nano.cnr.it>
2021-12-01 12:15:07 +0100

    Revert "corvina added"
    This reverts commit 6a040552c481f8a0eeaadf9271b567a2ff307179.

--- config/ROBOTS_list.pl
+++ config/ROBOTS_list.pl
@@ -47,4 +47,3 @@ $ROBOTS{'yurigagarin'} = 'yurigagarin.local';
 $ROBOTS{'attacc-lavoro'} = 'attacc-lavoro';
 $ROBOTS{'VivoBook'} = 'VivoBook';
 $ROBOTS{'AcerAspire'} = 'AcerAspire';
-$ROBOTS{'corvina'} = 'corvina.fisica.unimo.it';
