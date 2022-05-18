747b9e2c2d56366307ee0160e15c95c831fad07b
Davide Sangalli <davide.sangalli@gmail.com>
2022-05-18 11:25:10 +0200

    Added info for new procedure with abinit

--- TESTS/MAIN/hBN/EXCPH_abi/DFT/generate_dbs.sh
+++ TESTS/MAIN/hBN/EXCPH_abi/DFT/generate_dbs.sh
@@ -1,6 +1,10 @@
-ABINIT=abinit_9.2
-MRGDDB=mrgddb_9.2
-MRGDV=mrgdv_9.2
+ABINIT="abinit_9.6"
+MRGDDB="mrgddb_9.6"
+MRGDV="mrgdv_9.6"
+A2Y="a2y_a2y"
+
+rm -rf io_dir
+mkdir  io_dir
 
 echo "abinit step 1"
 $ABINIT < inputs/01hbn.files > 01hbn.log
@@ -18,8 +22,12 @@ $MRGDV  < inputs/03mrgdv.in  > 03mrgdv.log
 # Option 2
 cd io_dir
 ln -s hbn_out_DS2_WFK.nc hbn_out_DS2_WFQ.nc   # This could be replaced by an explicit calculation at k+q if needed
+ln -s hbn_out_DS2_WFK    hbn_out_DS2_WFQ
 cd ..
 
 echo "abinit final step"
 $ABINIT < inputs/04hbn_ddb.files > 04hbn_ddb.log
+
+echo "generate the yambo SAVE"
+
 
---
+++ TESTS/MAIN/hBN/EXCPH_abi/DFT/generate_dbs_new.sh
@@ -0,0 +1,21 @@
+ABINIT="abinit_9.6"
+MRGDDB="mrgddb_9.6"
+MRGDV="mrgdv_9.6"
+A2Y="a2y_a2y"
+
+rm -rf io_dir_new
+mkdir  io_dir_new
+
+echo "abinit step 1"
+$ABINIT < inputs_new/01hbn.files > 01hbn_new.log
+
+echo "merge databases"
+$MRGDDB < inputs_new/02mrgddb.in > 02mrgddb_new.log
+$MRGDV  < inputs_new/03mrgdv.in  > 03mrgdv_new.log
+
+echo "abinit final step"
+$ABINIT < inputs_new/04hbn_ddb.files > 04hbn_ddb_new.log
+
+echo "generate the yambo SAVE"
+
+
