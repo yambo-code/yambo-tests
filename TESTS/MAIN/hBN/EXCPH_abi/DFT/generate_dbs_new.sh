742ae4ce9db44621e565a76de3069b82c408aa16
Davide Sangalli <davide.sangalli@gmail.com>
2022-09-16 10:40:10 +0200

    working on elph interface with abinit, case without symmetries

--- TESTS/MAIN/hBN/EXCPH_abi/DFT/generate_dbs_new.sh
+++ TESTS/MAIN/hBN/EXCPH_abi/DFT/generate_dbs_new.sh
@@ -2,8 +2,8 @@ ABINIT="abinit_ibte"
 MRGDDB="mrgddb_ibte"
 MRGDV="mrgdv_ibte"
 A2Y="a2y_a2y"
-qkind="q_ibz"
-
+qkind="q_bz" # "q_ibz"
+expand="yes" # "no"
 
 if ! test -f io_dir_new/hbn_out_DS1_WFK.nc || ! test -f io_dir_new/hbn_out_DS2_WFK.nc ; then
   rm -rf io_dir_new
@@ -23,12 +23,19 @@ if ! test -f io_dir_new/hbn_${qkind}_out_GSTORE.nc ; then
   $ABINIT < inputs_new/04hbn_ddb_${qkind}.files > 04hbn_ddb_${qkind}_new.log
 fi
 
+WFK_file=hbn_out_DS2
+if [ $expand == "yes" ] ; then
+  echo "expand wfs"
+  $ABINIT < inputs_new/05_wfk_expand.in > 05_wfk_expand.log
+  WFK_file=hbn_wfk_expanded_out
+fi
+
 echo "generate the yambo SAVE with GKKP_abinit folder"
 cd io_dir_new
 rm hbn_out_DS2_GKKP.nc
-ln -s hbn_${qkind}_out_GSTORE.nc hbn_out_DS2_GKKP.nc
+ln -s hbn_${qkind}_out_GSTORE.nc ${WFK_file}_GKKP.nc
 rm -rf SAVE
-$A2Y -F hbn_out_DS2_WFK.nc
+$A2Y -F ${WFK_file}_WFK.nc
 mkdir ../yambo_${qkind}
 mv SAVE ../yambo_${qkind}
 mv GKKP_abinit ../yambo_${qkind}
