a857b712cbb6aa8eeba14ad0820d494decea1e3c
Davide Sangalli <davide.sangalli@gmail.com>
2023-03-14 12:26:36 +0100

    Small changes to scripts to generate elph tests

--- TESTS/MAIN/hBN/EXCPH/DFT/generate_DBs.sh
+++ TESTS/MAIN/hBN/EXCPH/DFT/generate_DBs.sh
@@ -1,8 +1,15 @@
+echo "pw.x_dev < input/hBN.scf.in > hBN.scf.out"
 pw.x_dev < input/hBN.scf.in > hBN.scf.out
+echo "pw.x_dev < input/hBN.nscf.in > hBN.nscf.out"
 pw.x_dev < input/hBN.nscf.in > hBN.nscf.out
+echo "ph.x_dev < input/hBN.phonon.in > hBN.phonon.out"
 ph.x_dev < input/hBN.phonon.in > hBN.phonon.out
+echo "ph.x_dev < input/hBN.dvscf.in > hBN.dvscf.out"
 ph.x_dev < input/hBN.dvscf.in > hBN.dvscf.out
-cd bn
+cd bn.save
+echo "p2y_excph"
 p2y_excph
+echo "yambo_excph  -F ../../MORE_inputs/01_setup"
 yambo_excph  -F ../../MORE_inputs/01_setup
+echo "ypp_ph_excph -F ../../MORE_inputs/02_gkkp_convert"
 ypp_ph_excph -F ../../MORE_inputs/02_gkkp_convert
--- TESTS/MAIN/hBN/EXCPH/DFT/read_gkkp.sh
+++ TESTS/MAIN/hBN/EXCPH/DFT/read_gkkp.sh
@@ -4,5 +4,5 @@ kind="qindx_C"
 
 ncdump_fx_t GKKP_$kind/ndb.elph_gkkp_expanded > GKKP_$kind/ndb.elph_gkkp_expanded.dat
 for iq in {1..9}; do
-  ncdump_fx_t GKKP_$kind/ndb.elph_gkkp_expanded_fragment_$iq > GKKP_$kind/ndb.elph_gkkp_expanded_fragment_$iq.dat
+  ncdump GKKP_$kind/ndb.elph_gkkp_expanded_fragment_$iq > GKKP_$kind/ndb.elph_gkkp_expanded_fragment_$iq.dat
 done
