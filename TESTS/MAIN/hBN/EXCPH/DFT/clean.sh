24cff8ba9cf220fab1eb1c7e69cf51515909d9a3
Davide Sangalli <davide.sangalli@gmail.com>
2023-07-14 09:44:28 +0200

    saving few local changes

---
+++ TESTS/MAIN/hBN/EXCPH/DFT/clean.sh
@@ -0,0 +1,1 @@
+rm -rf _ph0 elph_dir  *.out bn.* input_tmp.in
--- TESTS/MAIN/hBN/EXCPH/DFT/generate_DBs.sh
+++ TESTS/MAIN/hBN/EXCPH/DFT/generate_DBs.sh
@@ -1,15 +1,25 @@
-echo "pw.x_dev < input/hBN.scf.in > hBN.scf.out"
-pw.x_dev < input/hBN.scf.in > hBN.scf.out
-echo "pw.x_dev < input/hBN.nscf.in > hBN.nscf.out"
-pw.x_dev < input/hBN.nscf.in > hBN.nscf.out
-echo "ph.x_dev < input/hBN.phonon.in > hBN.phonon.out"
-ph.x_dev < input/hBN.phonon.in > hBN.phonon.out
-echo "ph.x_dev < input/hBN.dvscf.in > hBN.dvscf.out"
-ph.x_dev < input/hBN.dvscf.in > hBN.dvscf.out
+pw=pw.x_dev
+ph=ph.x_dev
+
+p2y=p2y_excph
+yambo=yambo_excph
+yambo_ph=yambo_ph_excph
+ypp_ph=ypp_ph_excph
+
+echo "$pw < input/hBN.scf.in > hBN.scf.out"
+$pw < input/hBN.scf.in > hBN.scf.out
+echo "$pw < input/hBN.nscf.in > hBN.nscf.out"
+$pw < input/hBN.nscf.in > hBN.nscf.out
+echo "$ph < input/hBN.phonon.in > hBN.phonon.out"
+$ph < input/hBN.phonon.in > hBN.phonon.out
+echo "ph < input/hBN.dvscf.in > hBN.dvscf.out"
+$ph < input/hBN.dvscf.in > hBN.dvscf.out
 cd bn.save
-echo "p2y_excph"
-p2y_excph
-echo "yambo_excph  -F ../../MORE_inputs/01_setup"
-yambo_excph  -F ../../MORE_inputs/01_setup
-echo "ypp_ph_excph -F ../../MORE_inputs/02_gkkp_convert"
-ypp_ph_excph -F ../../MORE_inputs/02_gkkp_convert
+echo "$p2y"
+$p2y
+echo "$yambo -F ../../MORE_inputs/00_setup"
+$yambo -F ../../MORE_inputs/00_setup
+echo "$yambo_ph  -F ../../MORE_inputs/01_setup"
+$yambo_ph  -F ../../MORE_inputs/01_setup
+echo "$ypp_ph -F ../../MORE_inputs/02_gkkp_convert"
+$ypp_ph -F ../../MORE_inputs/02_gkkp_convert -J GKKP -C GKKP
