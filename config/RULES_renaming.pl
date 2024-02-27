dac0295be5aee42fd954fcfe9d753d7bc9892233
Davide Sangalli <davide.sangalli@gmail.com>
2024-02-27 15:23:41 +0100

    Reference, renaming rules and input files fixed

--- config/RULES_renaming.pl
+++ config/RULES_renaming.pl
@@ -42,22 +42,22 @@ $PATTERN[$N_PATTERNS][1]="YPP-2D_occ_dos";
 $PATTERN[$N_PATTERNS][2]="YPP-TD_dos";
 #
 $N_PATTERNS++;
-$PATTERN_branch[$N_PATTERNS]="devel-nl phys/non-linear";
+$PATTERN_branch[$N_PATTERNS]="devel-nl phys/non-linear phys-non-linear";
 $PATTERN[$N_PATTERNS][1]="polarization_F";
 $PATTERN[$N_PATTERNS][2]="NL_pol_F";
 #
 $N_PATTERNS++;
-$PATTERN_branch[$N_PATTERNS]="devel-nl phys/non-linear";
+$PATTERN_branch[$N_PATTERNS]="devel-nl phys/non-linear phys-non-linear";
 $PATTERN[$N_PATTERNS][1]="current_F";
 $PATTERN[$N_PATTERNS][2]="NL_curr_F";
 #
 $N_PATTERNS++;
-$PATTERN_branch[$N_PATTERNS]="devel-nl phys/non-linear";
+$PATTERN_branch[$N_PATTERNS]="devel-nl phys/non-linear phys-non-linear";
 $PATTERN[$N_PATTERNS][1]="e_total_F";
 $PATTERN[$N_PATTERNS][2]="NL_Etot_F";
 #
 $N_PATTERNS++;
-$PATTERN_branch[$N_PATTERNS]="devel-nl phys/non-linear";
+$PATTERN_branch[$N_PATTERNS]="devel-nl phys/non-linear phys-non-linear";
 $PATTERN[$N_PATTERNS][1]="external_potential_F";
 $PATTERN[$N_PATTERNS][2]="NL_ext_pot_F";
 #
