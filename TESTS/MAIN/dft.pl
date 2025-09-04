f2fa8ef2027a1d482ee749866a6e1928733660a3
Andrea Marini <andrea.marini@cnr.it>
2025-09-04 18:48:08 +0200

    Working on materials

--- TESTS/MAIN/dft.pl
+++ TESTS/MAIN/dft.pl
@@ -12,24 +12,70 @@ use Net::Domain qw(hostname hostfqdn hostdomain domainname);
 $pwd=abs_path();
 $hostname=hostname();
 #
+$DIR="DFT";
 find({ wanted => \&process_dir, no_chdir => 1 }, ".");
-sub process_dir {
- if (-d $_ and $_ =~ /DFT/) {
-  $last=(split("/",$_))[-1];
-  if ( $last eq "DFT" ) { push @dirs,$_};
- };
-}
+@DFT_dirs=@dirs;
 #
-for $dir (@dirs) {
+$DIR="REFERENCE";
+@dirs=();
+find({ wanted => \&process_dir, no_chdir => 1 }, ".");
+@REF_dirs=@dirs;
+#
+for $dir (@REF_dirs) {
+ @files=( );
+ $MATERIAL=(split("/REFERENCE",$dir))[0];
+ $KIND="r-";
+ find({ wanted => \&process_files, no_chdir => 0 }, "$dir");
+ @in_files=@files;
+ $dft="no";
+ if (-d "$dir/../DFT") {
+  @files=( );
+  $KIND="\.files";
+  find({ wanted => \&process_files, no_chdir => 0 }, "$dir/../DFT");
+  $dft="ABINIT";
+  @dft_files=@files;
+  if( int(@dft_files) == 0) {$dft="PWSCF"};
+ };
+ $fh = IO::File->new( "$dir/$in_files[1]",'<');
+ $metal="no";
+ while ($line = <$fh>) {
+  chomp($line); 
+  if ($line =~ /Spin polarizations/){$N_spin=(split(":",$line))[-1]};
+  if ($line =~ /Spinor components/){$N_spinors=(split(":",$line))[-1]};
+  if ($line =~ /Compatible Grid/){$N_d=(split(" ",$line))[3]; if ($N_d eq ":") {$N_d=(split(" ",$line))[4]} };
+  if ($line =~ /Metallic system/){$metal="yes"};
+ }
+ $N_d =~ s/D//;
+ if ( $dft eq "no") {next};
+ if ( "$dft" eq "ABINIT" and $N_d == 3)
+ {
+  print "Material       : $MATERIAL\n";
+  print "DFT            : $dft\n";
+  print "Spin components: $N_spin\n";
+  print "Spinors        : $N_spinors\n";
+  print "Dimension      : $N_d\n";
+  print "Metallic       : $metal\n";
+ }
+}
+die;
+for $dir (@DFT_dirs) {
  @files=( );
  find({ wanted => \&process_files, no_chdir => 0 }, "$dir");
- sub process_files {
-  if (-f $_ and $_ =~ /in/ ) { push @files,$_};
+# print "\n DIRECTORY: $dir\n";
+# for my $file (@files) {
+#  print "file: $file\n";
+# }
+}
+
+sub process_files {
+ if (-f $_ and $_ =~ /$KIND/ ) { push @files,$_};
+};
+
+sub process_dir {
+ if (-d $_ and $_ =~ /$DIR/) {
+  $last=(split("/",$_))[-1];
+  if ( $last eq $DIR ) { push @dirs,$_};
  };
- print "\n DIRECTORY: $dir\n";
- for my $file (@files) {
-  print "file: $file\n";
- }
 }
 
 #$options="-azri";
